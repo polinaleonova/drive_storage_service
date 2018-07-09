63f4510884ec10ad02e8f39c1f9d0500a22d5082
polina <polina.ostapenko.88@gmail.com>
2018-07-09 22:47:49 +0300

    add logic for quick demo in console

--- README.md
+++ README.md
@@ -6,15 +6,23 @@ Access to the internet and a web browser.
 A Google account with Google Drive enabled.
 
 Step 1: Turn on the Drive API
-Use this wizard - https://console.developers.google.com/start/api?id=drive to create or select a project in the Google Developers Console and automatically turn on the API. Click Continue, then Go to credentials.
+Use this wizard - https://console.developers.google.com/start/api?id=drive to create or
+select a project in the Google Developers Console and automatically turn on the API. Click Continue, then Go to credentials.
 On the Add credentials to your project page, click the Cancel button.
-At the top of the page, select the OAuth consent screen tab. Select an Email address, enter a Product name if not already set, and click the Save button.
+At the top of the page, select the OAuth consent screen tab. Select an Email address, enter a Product name if not already set,
+and click the Save button.
 Select the Credentials tab, click the Create credentials button and select OAuth client ID.
-Select the application type Other, enter the name "Drive API Quickstart", and click the Create button.
+Select the application type Other, enter the name "your application name", and click the Create button.
 Click OK to dismiss the resulting dialog.
 Click the file_download (Download JSON) button to the right of the client ID.
 Create directory '.credentials' and add path to this directory in CREDENTIAL_DIR
-Move json file to this directory and rename it client_secret.json.
+Move json file to this directory and rename it to client_secret.json.
+To enable the Drive API, complete these steps:
+
+-Go to the Google API Console.
+-Select a project.
+-In the sidebar on the left, expand APIs & auth and select APIs.
+-In the displayed list of available APIs, click the link for the Drive API and click Enable API.
 
 Step 2: Install the Google Client Library
 Run the following command to install the library using pip:
@@ -24,18 +32,28 @@ pip install --upgrade google-api-python-client
 
 After all this steps, when you run firstly you application:
 1. run in console: python3 google_drive_service.py --noauth_local_webserver
-first time you get this in console:
+You will see in your console output:
+
+Init google drive client...
+Type name of folder in google drive
+for your application data or press Enter if you want to use default name
+>>>
+
+2. Type existing folder name from your google drive or
+ new name (new folder will be created on drive) or press Enter for using DEFAULT_STORAGE_NAME from settings.py
+
+first time you get this message in console:
 Go to the following link in your browser:
 
     https://accounts.google.com/o/oauth2/auth?redirect_uri=...
 
-Enter verification code:
+2.Enter verification code:
 
+- You should go to this link in browser and check account (which related with you application)
+- than you should choose 'Agree' with proposals to share you data with application "your application name"
+- Than you get code, and you should to fill this code in console input 'Enter verification code:'
+- You will get:
+Authentication successful. You can see new file storage.json in your CREDENTIAL_DIR.
 
-2. You should go to this link in browser and check account (which related with you application)
-3. than you should choose 'Agree' with proposals to share you data with application "Video Monitoring Storage"
-4. Than you get code, and you should to fill this code in console input 'Enter verification code:'
-5. You will get:
-Authentication successful.
-
-After this your application folder will be created in drive. And app can work with this folder's data.
+After this your application folder will be selected for uploading files or created in drive.
+ And app can work with this folder's data.
--- google_drive_service.py
+++ google_drive_service.py
@@ -5,12 +5,12 @@ from oauth2client import file, client, tools
 from httplib2 import Http
 from googleapiclient.http import MediaFileUpload
 from settings import CREDENTIAL_DIR, SCOPES, CLIENT_SECRET_FILE, \
-    DRIVE_STORAGE_NAME
+    DEFAULT_STORAGE_NAME
 
 
 class AppDriveApiClient(object):
 
-    def __init__(self):
+    def __init__(self, custom_name_folder=None):
         # client authorization
         if not os.path.exists(CREDENTIAL_DIR):
             os.makedirs(CREDENTIAL_DIR)
@@ -32,33 +32,42 @@ class AppDriveApiClient(object):
         # requests will be made through.
         self.service = discovery.build('drive', 'v3',
                                        http=creds.authorize(Http()))
-        self.storage_id = self._choose_or_create_storage_dir()
+        self.storage_id = self._choose_or_create_storage(custom_name_folder)
 
-    def _choose_or_create_storage_dir(self):
+    def _choose_or_create_storage(self, custom_name_folder=None):
         """
         Choose or Creation folder for storing application data. Return id
         of this folder.
+        :param custom_name_folder: if None - use default folder
+            'Video Monitoring System Storage', else string with your
+            custom folder name
         :return: string with folder id
         """
+        app_folders = []
+        self.name_folder = custom_name_folder or DEFAULT_STORAGE_NAME
+
         try:
-            app_folder = self.service.files().list(
-                q="mimeType='application/vnd.google-apps.folder' and name='Video Monitoring System Storage'").execute().get(
-                'files', [])
+            app_folders = self.service.files().list(
+                q="mimeType='application/vnd.google-apps.folder' and name='" +
+                  + self.name_folder+"'").execute().get('files', [])
         except Exception as e:
             print('Something went wrong on choosing application folder', e)
-        if not app_folder:
 
+        if not app_folders:
             file_metadata = {
-                'name': 'Video Monitoring System Storage',
+                'name': self.name_folder,
                 'mimeType': 'application/vnd.google-apps.folder'
             }
             try:
                 app_folder = self.service.files().create(body=file_metadata,
                                                          fields='id').execute()
+                storage_id = app_folder.get('id')
             except Exception as e:
                 print('Something went wrong on creation application folder', e)
-            print('Application folder was created on the drive')
-        storage_id = app_folder[0].get('id')
+            print("Application folder '{}' was created on the drive".format(
+                self.name_folder))
+        else:
+            storage_id = app_folders[0].get('id')
         return storage_id
 
     def show_app_files_list(self):
@@ -69,40 +78,51 @@ class AppDriveApiClient(object):
         files = self.service.files().list(
             spaces='drive').execute().get('files', [])
         if len(files) > 1:
-            print('Video Monitoring System Storage content:')
+            print(self.name_folder + ' content:')
+            for f in files:
+                if not f['mimeType'] == 'application/vnd.google-apps.folder':
+                    print(f['name'], f['mimeType'])
         else:
-            print('Video Monitoring System Storage is empty')
-        for file in files:
-            if not file['mimeType'] == 'application/vnd.google-apps.folder':
-                print(file['name'], file['mimeType'])
+            print(self.name_folder + ' is empty')
 
     def upload_file_to_drive(self, path_to_file, new_file_name):
         """
-        Uploading file from current path in google drive application
+        Uploading file from current path to google drive application
         data folder.
-        :param path_to_file: string with path;
+        :param path_to_file: string with absolute path to file;
         new_file_name: string
         """
-        file_metadata = {'name': new_file_name, 'parents': [self.storage_id]}
-        media = MediaFileUpload(path_to_file)
-        self.service.files().create(body=file_metadata, media_body=media,
-                                    fields='id').execute()
-        print('File %s was uploaded' % new_file_name)
+        if not os.path.exists(path_to_file):
+            print("File '" + path_to_file + "' not found")
+        else:
+            file_name = new_file_name or os.path.basename(
+                path_to_file.split('.')[0])  # without extension
+            file_metadata = {'name': file_name, 'parents': [self.storage_id]}
+            media = MediaFileUpload(path_to_file)
+            self.service.files().create(body=file_metadata, media_body=media,
+                                        fields='id').execute()
+            print('File %s was uploaded to storage' % file_name)
 
     def upload_files_to_drive(self, path_to_dir):
         """
-        Uploading all files from some directory in google drive application
+        Uploading all files from some directory to google drive application
         data folder.
-        :param path_to_dir: string with path
+        :param path_to_dir: string with absolute path to directory
 
         """
-        for file in os.listdir(path_to_dir):
-            path_to_file = os.path.join(path_to_dir, file)
-            file_metadata = {'name': file, 'parents': [self.storage_id]}
-            media = MediaFileUpload(path_to_file)
-            self.service.files().create(body=file_metadata,
-                                        media_body=media,
-                                        fields='id').execute()
+        count = 0
+        if not os.path.isdir(path_to_dir):
+            print("Directory '" + path_to_dir + "' not found")
+        else:
+            for f in os.listdir(path_to_dir):
+                path_to_file = os.path.join(path_to_dir, f)
+                file_metadata = {'name': f, 'parents': [self.storage_id]}
+                media = MediaFileUpload(path_to_file)
+                self.service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields='id').execute()
+                count += 1
+            print(str(count) + " files were uploaded to application storage")
 
     def clean_app_folder(self):
         """
@@ -111,8 +131,42 @@ class AppDriveApiClient(object):
         """
         files = self.service.files().list(
             spaces='drive').execute().get('files', [])
-        for file in files:
-            if not file['mimeType'] == 'application/vnd.google-apps.folder':
-                file_id = file['id']
+        count = 0
+        for f in files:
+            if not f['mimeType'] == 'application/vnd.google-apps.folder':
+                file_id = f['id']
                 self.service.files().delete(fileId=file_id).execute()
-                print('File %s was removed from Video Monitoring System Storage' % file['name'])
+                print("File %s was removed from " + self.name_folder % f['name'])
+                count += 1
+        print(str(count) + " files were removed from application storage")
+
+
+# demo
+if __name__ == "__main__":
+    print("Init google drive client...")
+    drive_fold_name = input("Type name of folder in google drive\
+                         for your application data or press Enter\
+                          if you want to use default name\n>>>")
+    drive_client = AppDriveApiClient(drive_fold_name)
+    drive_client.show_app_files_list()
+
+    filepath = input(
+        "Type absolute path to your local file for uploading\n>>>")
+
+    filename = input(
+        "Type new name (without extension) for uploaded\
+         file or press Enter for using current name\n>>>")
+    drive_client.upload_file_to_drive(filepath, filename)
+
+    path_to_dir = input(
+        "Type absolute path to your local directory for \
+        content uploading\n>>>")
+    drive_client.upload_files_to_drive(path_to_dir)
+
+    drive_client.show_app_files_list()
+
+    clean_storage = input(
+        "Do you want to remove all files from current\
+         application storage? y/n\n>>>")
+    if clean_storage == 'y':
+        drive_client.clean_app_folder()
